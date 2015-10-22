3540e44080eda5223ab9652af19eeb9ae5aed95b
Santiago Sinisterra <santiago.sinisterra.s@gmail.com>
2015-10-21 23:07:23 -0500

    refactor infoPersonal, agregar logo a dashboard, CRUD certificaciones

--- src/app/info/infoPersonal.controller.js
+++ src/app/info/infoPersonal.controller.js
@@ -8,162 +8,40 @@
 	/* @ngInject */
 	function InfoPersonalController(Restangular, $filter, toastr, _) {
 		var vm = this;
-		vm.title = 'InfoPersonalController';
 
+		// VALIDAR FECHAS
 		vm.today = new Date();
 
-		activate();
-
+		// PERSONAL/DIRECCIÓN
 		vm.submitFormPersonal = submitFormPersonal;
-		vm.regexCURP = /^[A-Z]{1}[AEIOU]{1}[A-Z]{2}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[HM]{1}(AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[B-DF-HJ-NP-TV-Z]{3}[0-9A-Z]{1}[0-9]{1}$/;
 
+		// EXPERIENCIA LABORAL
 		vm.submitFormExperiencia = submitFormExperiencia;
-
-		// experiencia laboral
-
 		vm.editExperiencia = editExperiencia;
 		vm.removeExperiencia = removeExperiencia;
+		vm.regexCURP = /^[A-Z]{1}[AEIOU]{1}[A-Z]{2}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[HM]{1}(AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[B-DF-HJ-NP-TV-Z]{3}[0-9A-Z]{1}[0-9]{1}$/;
 
-		function editExperiencia(exp) {
-			//escape values
-			vm.experiencia = exp;
+		// CERTIFICACIONES
+
+		vm.submitFormCerts = submitFormCerts;
+		vm.editCert = editCert;
+		vm.removeCert = removeCert;
+
+		// ESTADOS Y MUNICIPIOS
+		function loadLocations() {
+			vm.locationsLoaded = false;
+			Restangular.all('Pais').customGET().then(function(res) {
+				vm.states = res.paises[0].entidades_federativas;
+
+				vm.locationsLoaded = true;
+
+			}).catch(function(err) {
+				vm.locationsLoaded = true;
+
+			});
 		}
 
-		function removeExperiencia(exp) {
-			Restangular.one('ExperienciaLaboral', exp.id).remove().then(function(res) {
-					vm.experienciaLaboral = _.without(vm.experienciaLaboral, exp);
-				})
-				.catch(function() {
-					toastr.error('Hubo un error', 'Error');
-				});
-		}
-
-		function submitFormPersonal(isValid) {
-
-			if (isValid) {
-
-				//proceder a enviar la forma
-
-				vm.savingPersonal = true;
-
-				savePersonal();
-
-			}
-		}
-
-		function submitFormExperiencia(isValid) {
-			if (isValid) {
-				saveExperiencia();
-			}
-		}
-
-		function savePersonal() {
-			var personal = angular.copy(vm.personal);
-			var saveUrl;
-
-			personal.FechaNacimiento = $filter('date')(vm.personal.FechaNacimiento, 'yyyy-MM-dd');
-
-			if (personal.id) {
-				delete personal.id;
-				delete personal.idUsuario;
-				saveUrl = Restangular.all('DatosPersonales').customPUT(personal);
-			}
-			else {
-				saveUrl = Restangular.all('DatosPersonales').customPOST(personal);
-			}
-
-			saveUrl.then(function(res) {
-					toastr.success('La información ha sido guardada correctamente.', '¡Éxito!');
-					vm.savingPersonal = false;
-					saveDireccion();
-				})
-				.catch(function(error) {
-					toastr.error('La información no pudo guardarse correctamente ' + '(' + error.status + ')', 'Error');
-					vm.savingPersonal = false;
-
-				});
-		}
-
-		function saveDireccion() {
-			var direccion = angular.copy(vm.direccion);
-			var saveUrl;
-
-			if (direccion.id) {
-				delete direccion.id;
-				delete direccion.idUsuario;
-				saveUrl = Restangular.all('Direccion').customPUT(direccion);
-			}
-			else {
-				direccion.idUsuario = vm.personal.idUsuario;
-				saveUrl = Restangular.all('Direccion').customPOST(direccion);
-			}
-
-			saveUrl.then(function(res) {
-					// toastr.success('La información ha sido guardada correctamente', '¡Éxito!');
-					vm.savingPersonal = false;
-				})
-				.catch(function(error) {
-					toastr.error('La información no pudo guardarse correctamente ' + '(' + error.status + ')', 'Error');
-					vm.savingPersonal = false;
-
-				});
-
-		}
-
-		function formatExperiencia(e) {
-			e.FechaInicio = new Date(e.FechaInicio);
-			e.FechaTermino = new Date(e.FechaTermino);
-			e.RemuneracionBrutaMensual = Number(e.RemuneracionBrutaMensual);
-			e.TelefonoSuperiorInmediato = Number(e.TelefonoSuperiorInmediato);
-
-			return e;
-		}
-
-		function saveExperiencia() {
-			var experiencia = angular.copy(vm.experiencia);
-
-			// if(experiencia.idExperienciaEspecifica){
-			// 	//editar
-			// }
-			// else{
-
-			delete experiencia.campo;
-			delete experiencia.area;
-			experiencia.FechaInicio = $filter('date')(experiencia.FechaInicio, 'yyyy-MM-dd');
-			experiencia.FechaTermino = $filter('date')(experiencia.FechaTermino, 'yyyy-MM-dd');
-
-			if (experiencia.id) {
-				Restangular.one('ExperienciaLaboral', experiencia.id).customPUT(experiencia).then(function(res) {
-					// vm.experienciaLaboral.push(formatExperiencia(res.plain()));
-					//BUSCAR EN vm.experiencia laboral y reemplazar
-					toastr.success('Cambios guardados correctamente', '¡Éxito!');
-				});
-			}
-			else {
-				Restangular.all('ExperienciaLaboral').customPOST(experiencia).then(function(res) {
-						vm.experienciaLaboral.push(formatExperiencia(res.plain()));
-						toastr.success('Cambios guardados correctamente', '¡Éxito!');
-					})
-					.catch(function() {
-
-					});
-				// vm.experiencia = {};
-			}
-
-		}
-
-		////////////////
-
-		function activate() {
-			vm.today = new Date();
-			loadLocations();
-			loadExperienceFields();
-
-			loadPersonal();
-			loadDireccion();
-			loadExperiencia();
-		}
-
+		// CAMPOS, ÁREAS Y EXPERIENCIAS
 		function loadExperienceFields() {
 			Restangular.all('CampoDeExperiencia').customGET().then(function(res) {
 
@@ -195,19 +73,7 @@
 				});
 		}
 
-		function loadLocations() {
-			vm.locationsLoaded = false;
-			Restangular.all('Pais').customGET().then(function(res) {
-				vm.states = res.paises[0].entidades_federativas;
-
-				vm.locationsLoaded = true;
-
-			}).catch(function(err) {
-				vm.locationsLoaded = true;
-
-			});
-		}
-
+		// PERSONAL
 		function loadPersonal() {
 			vm.personalLoaded = false;
 
@@ -226,6 +92,47 @@
 				});
 		}
 
+		function savePersonal() {
+			var personal = angular.copy(vm.personal);
+			var saveUrl;
+
+			personal.FechaNacimiento = $filter('date')(vm.personal.FechaNacimiento, 'yyyy-MM-dd');
+
+			if (personal.id) {
+				delete personal.id;
+				delete personal.idUsuario;
+				saveUrl = Restangular.all('DatosPersonales').customPUT(personal);
+			}
+			else {
+				saveUrl = Restangular.all('DatosPersonales').customPOST(personal);
+			}
+
+			saveUrl.then(function(res) {
+					toastSuccess();
+					vm.savingPersonal = false;
+					saveDireccion();
+				})
+				.catch(function(error) {
+					toastError(error);
+					vm.savingPersonal = false;
+
+				});
+		}
+
+		function submitFormPersonal(isValid) {
+
+			if (isValid) {
+
+				//proceder a enviar la forma
+
+				vm.savingPersonal = true;
+
+				savePersonal();
+
+			}
+		}
+
+		// DIRECCIÓN
 		function loadDireccion() {
 			Restangular.all('Direccion').customGET().then(function(res) {
 					vm.direccion = res.plain().direccion;
@@ -235,6 +142,8 @@
 				});
 		}
 
+
+		// EXPERIENCIA LABORAL
 		function loadExperiencia() {
 			vm.experienciaLoaded = false;
 			vm.experienciaLaboral = [];
@@ -256,5 +165,152 @@
 					vm.experienciaLoaded = true;
 				});
 		}
+
+		function formatExperiencia(e) {
+			e.FechaInicio = new Date(e.FechaInicio);
+			e.FechaTermino = new Date(e.FechaTermino);
+			e.RemuneracionBrutaMensual = Number(e.RemuneracionBrutaMensual);
+			e.TelefonoSuperiorInmediato = Number(e.TelefonoSuperiorInmediato);
+
+			return e;
+		}
+
+		function saveExperiencia() {
+			var experiencia = angular.copy(vm.experiencia);
+
+			// if(experiencia.idExperienciaEspecifica){
+			// 	//editar
+			// }
+			// else{
+
+			delete experiencia.campo;
+			delete experiencia.area;
+			experiencia.FechaInicio = $filter('date')(experiencia.FechaInicio, 'yyyy-MM-dd');
+			experiencia.FechaTermino = $filter('date')(experiencia.FechaTermino, 'yyyy-MM-dd');
+
+			if (experiencia.id) {
+				Restangular.one('ExperienciaLaboral', experiencia.id).customPUT(experiencia).then(function(res) {
+					// vm.experienciaLaboral.push(formatExperiencia(res.plain()));
+					//BUSCAR EN vm.experiencia laboral y reemplazar
+					toastSuccess();
+				});
+			}
+			else {
+				Restangular.all('ExperienciaLaboral').customPOST(experiencia).then(function(res) {
+						vm.experienciaLaboral.push(formatExperiencia(res.plain()));
+						toastSuccess();
+					})
+					.catch(function() {
+
+					});
+				// vm.experiencia = {};
+			}
+		}
+
+		function submitFormExperiencia(isValid) {
+			if (isValid) {
+				saveExperiencia();
+			}
+		}
+
+		function editExperiencia(exp) {
+			//escape values
+			vm.experiencia = exp;
+		}
+
+		function removeExperiencia(exp) {
+			Restangular.one('ExperienciaLaboral', exp.id).remove().then(function(res) {
+					vm.experienciaLaboral = _.without(vm.experienciaLaboral, exp);
+				})
+				.catch(function(error) {
+					toastError(error);
+				});
+		}
+
+		// CERTIFICACIONES
+
+		function loadCerts(){
+			vm.certLoaded = false;
+			Restangular.all('Certificacion').customGET().then(function(res){
+				vm.certs = res.plain().certificacion;
+				vm.certLoaded = true;
+			})
+			.catch(function(){
+				vm.certLoaded = true;
+			});
+		}
+
+		function saveCert(){
+			var cert = angular.copy(vm.cert);
+			var saveUrl;
+			vm.savingCert = true;
+
+			if(cert.id){
+				saveUrl = Restangular.one('Certificacion', cert.id).customPUT(cert).then(function(res){
+					toastSuccess();
+					vm.savingCert = false;
+				})
+				.catch(function(err){
+					toastError(err);
+					vm.savingCert = false;
+				});
+			}
+			else{
+				saveUrl = Restangular.all('Certificacion').customPOST(cert).then(function(res){
+					toastSuccess();
+					vm.certs.push(res.plain());
+					vm.savingCert = false;
+				})
+				.catch(function(err){
+					toastError(err);
+					vm.savingCert = false;
+				});
+			}
+		}
+
+		function submitFormCerts(isValid){
+			if(isValid){
+				saveCert();
+			}
+		}
+
+		function editCert(cert){
+			vm.cert = cert;
+		}
+
+		function removeCert(cert){
+			Restangular.one('Certificacion', cert.id).remove().then(function(){
+				toastSuccess();
+				vm.certs = _.without(vm.certs, cert);
+			})
+			.catch(function(err){
+				toastError(err);
+			});
+		}
+
+
+		////////////////
+
+		function activate() {
+			vm.today = new Date();
+			loadLocations();
+			loadExperienceFields();
+
+			loadPersonal();
+			loadDireccion();
+			loadExperiencia();
+			loadCerts();
+		}
+
+		function toastSuccess(){
+			toastr.success('La información se ha guardado correctamente.', '¡Éxito!');
+		}
+
+		function toastError(err){
+			toastr.error('Hubo un error al guardar la información ('+err.status+')', 'Error');
+		}
+
+		activate();
+
 	}
 })();
